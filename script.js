c2488ce4e9eaf249c72856d7fafb811bb08bf4b4
darklord7354 <90344737+darklord7354@users.noreply.github.com>
2021-09-09 16:34:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const name = prompt("what is your first name in small letters please")
+
+if (name === "ira") {
+    document.getElementById("potato").textContent = "Ira Umesh Khot, You do not have any roles as of now this will be updated in future website changes so please wait"
+} else if (name === "nathan") {
+    document.getElementById("potato").textContent = "Nathan Muturi Hunja, You do not have any roles as of now this will be updated in future website changes so please wait"
+} else if (name === "nisarg") {
+    document.getElementById("potato").textContent = "Nisarg Niraj Maru, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "sevdip") {
+    document.getElementById("potato").textContent = "Sevdip Singh Reel, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "shamari") {
+    document.getElementById("potato").textContent = "Shamari Baraka Gioko, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "leo") {
+    document.getElementById("potato").textContent = "Leo Keli Mutuku, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "shreya") {
+    document.getElementById("potato").textContent = "Shreya Anilkumar Nagda, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "neema") {
+    document.getElementById("potato").textContent = "Neema Bosibori Gunga, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "christopher") {
+    document.getElementById("potato").textContent = "Christopher Mathenge Mwangi, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "suki") {
+    document.getElementById("potato").textContent = "Suki Minayo Anyonge, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "akshi") {
+    document.getElementById("potato").textContent = "Akshi Pragnesh Chheda, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "krish") {
+    document.getElementById("potato").textContent = "Krish Utpalkumar Patel, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else if (name === "sahildev") {
+    document.getElementById("potato").textContent = "Sahildev Surjit Singh, You do not have any roles as of now this will be changed in future website changes so please wait"
+} else {
+    document.getElementById("potato").textContent = "Sorry, " + name + " we cannot find your name or it must be a typo or you must have entered your name with a capital letter if this does not work please contact darklord7.inquiries@gmail.com for further support we will reply as soon as possible,Thank you."
+}
